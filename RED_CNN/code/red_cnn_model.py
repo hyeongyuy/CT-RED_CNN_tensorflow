db01c34010df315be8ce901fad6dd8005dca131f
hyeongyu <hyeongyu5@gmail.com>
2018-10-27 14:40:20 +0900

    upload

--- RED_CNN/code/red_cnn_model.py
+++ RED_CNN/code/red_cnn_model.py
@@ -47,7 +47,7 @@ class redCNN(object):
             self.test_image_loader(self.test_patent_no)
             print('data load complete !!!, {}, N_test : {}'.format(time.time() - t1, len(self.test_image_loader.LDCT_image_name)))
             self.X = tf.placeholder(tf.float32, [None, args.patch_size, args.patch_size, args.img_channel], name = 'LDCT')
-            self.Y = tf.placeholder(tf.float32, [None, args.patch_size, args.patch_size, args.img_channel], name = 'NDCT')
+            self.Y = tf.placeholder(tf.float32, [None, args.patch_size, args.patch_size, args.img_channel], name = 'LDCT')
         
         """
         build model
@@ -85,17 +85,6 @@ class redCNN(object):
         self.summary_train_image = tf.summary.image('0_train_image', self.check_img_summary)                                    
         self.whole_img_summary = tf.concat([self.whole_X, self.whole_Y, self.WHOLE_output_img], axis = 2)        
         self.summary_image = tf.summary.image('1_whole_image', self.whole_img_summary)
-
-        #ROI summary
-        if args.mayo_roi:
-            self.ROI_X =  tf.placeholder(tf.float32, [None, 128, 128, args.img_channel], name='ROI_X')
-            self.ROI_Y =  tf.placeholder(tf.float32, [None, 128, 128, args.img_channel], name='ROI_Y')
-            self.ROI_output = self.red_cnn(self.ROI_X)
-
-            self.ROI_real_img_summary = tf.concat([self.ROI_X, self.ROI_Y, self.ROI_output], axis = 2)
-            self.summary_ROI_image_1 = tf.summary.image('2_ROI_image_1', self.ROI_real_img_summary)
-            self.summary_ROI_image_2 = tf.summary.image('3_ROI_image_2', self.ROI_real_img_summary)
-        
 
                 
         #model saver
@@ -158,56 +147,6 @@ class redCNN(object):
                                             })
         self.writer.add_summary(summary_str1, t)
         self.writer.add_summary(summary_str2, t)
-
-        """
-            summary ROI IMAGE
-        """
-        if args.mayo_roi:
-            ROI_sample = [['067', '0203', [161, 289], [61, 189]],
-                        ['291', '0196', [111, 239], [111, 239]]]
-            
-            LDCT_ROI_idx = [self.test_image_loader.LDCT_image_name.index(\
-                'L{}_{}_{}'.format(s[0], args.LDCT_path, s[1])) for s in ROI_sample]
-            
-            NDCT_ROI_idx = [self.test_image_loader.NDCT_image_name.index(\
-                'L{}_{}_{}'.format(s[0], args.NDCT_path, s[1])) for s in ROI_sample]
-
-
-            RIO_LDCT  = [self.test_image_loader.LDCT_images[idx] for idx in LDCT_ROI_idx]
-            RIO_NDCT  = [self.test_image_loader.NDCT_images[idx] for idx in NDCT_ROI_idx]
-
-            ROI_LDCT_arr = [ut.ROI_img(RIO_LDCT[0], row = ROI_sample[0][2], col = ROI_sample[0][3]), \
-                            ut.ROI_img(RIO_LDCT[1], row = ROI_sample[1][2], col = ROI_sample[1][3])]
-
-            ROI_NDCT_arr = [ut.ROI_img(RIO_NDCT[0], row = ROI_sample[0][2], col = ROI_sample[0][3]), \
-                            ut.ROI_img(RIO_NDCT[1], row = ROI_sample[1][2], col = ROI_sample[1][3])]
-            
-            ROI_output_1 = self.sess.run(
-                self.ROI_output,  feed_dict={
-                self.ROI_X :  ROI_LDCT_arr[0].reshape([1] + self.ROI_X.get_shape().as_list()[1:])})
-
-            ROI_output_2 = self.sess.run(
-                self.ROI_output,  feed_dict={
-                self.ROI_X :  ROI_LDCT_arr[1].reshape([1] + self.ROI_X.get_shape().as_list()[1:])})
-
-
-            ROI_output_1, ROI_output_2 = np.array(ROI_output_1).astype(np.float32), np.array(ROI_output_2).astype(np.float32)
-
-            roi_summary_str1 = self.sess.run(
-                self.summary_ROI_image_1,
-                feed_dict={self.ROI_X : ROI_LDCT_arr[0].reshape([1] + self.ROI_X.get_shape().as_list()[1:]), 
-                           self.ROI_Y : ROI_NDCT_arr[0].reshape([1] + self.ROI_Y.get_shape().as_list()[1:]),
-                           self.ROI_output: ROI_output_1.reshape([1] + self.ROI_output.get_shape().as_list()[1:])})
-
-            roi_summary_str2  = self.sess.run(
-                self.summary_ROI_image_2,
-                feed_dict={self.ROI_X : ROI_LDCT_arr[1].reshape([1] + self.ROI_X.get_shape().as_list()[1:]), 
-                           self.ROI_Y : ROI_NDCT_arr[1].reshape([1] + self.ROI_Y.get_shape().as_list()[1:]),
-                           self.ROI_output: ROI_output_2.reshape([1] + self.ROI_output.get_shape().as_list()[1:])})
-
-            self.writer.add_summary(roi_summary_str1, t)
-            self.writer.add_summary(roi_summary_str2, t)
-
 
     
     def save(self, step, checkpoint_dir = 'checkpoint'):
